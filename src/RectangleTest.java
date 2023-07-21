4fe83ef53565cb083c2b9f950e0d0b5d77f9a542
aminaryan80 <aryanmohammadamin@gmail.com>
2023-07-21 20:45:27 +0800

    feat: Add tests for flexible rectangle feature

--- src/RectangleTest.java
+++ src/RectangleTest.java
@@ -9,4 +9,20 @@ class RectangleTest {
         Rectangle r = new Rectangle(4, 5);
         assertEquals(20, r.computeArea());
     }
+
+    @Test
+    void setWidth() {
+        Rectangle r = new Rectangle(4, 5);
+        assertEquals(20, r.computeArea());
+        r.setWidth(10);
+        assertEquals(50, r.computeArea());
+    }
+
+    @Test
+    void setHeight() {
+        Rectangle r = new Rectangle(4, 5);
+        assertEquals(20, r.computeArea());
+        r.setHeight(10);
+        assertEquals(40, r.computeArea());
+    }
 }
