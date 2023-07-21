149e2bbf4f67c86395223abe2bd899716e4d77ac
aminaryan80 <aryanmohammadamin@gmail.com>
2023-07-21 20:38:17 +0800

    feat: Add logic for immutable rectangle feature

---
+++ src/Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle {
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int computeArea() {
+        return this.width * this.height;
+    }
+}
